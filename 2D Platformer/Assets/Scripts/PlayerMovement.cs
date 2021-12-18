aac43714f693fc6bf3882e66204c1045809d3e44
TinoCederholm <42689.@edu.raseko.fi>
2021-12-18 08:05:21 +0200

    Added idle&walking animations to player character

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -23,11 +25,21 @@ public class PlayerMovement : MonoBehaviour
       change= Vector3.zero;
       change.x = Input.GetAxisRaw("Horizontal");
       change.y = Input.GetAxisRaw("Vertical");
-      if(change != Vector3.zero)
+      updateAnimationAndMove();
+
+    }
+
+    void updateAnimationAndMove()
+    {
+            if(change != Vector3.zero)
       {
         MoveCharacter();
+        animator.SetFloat("moveX", change.x);
+        animator.SetFloat("moveY", change.y);
+        animator.SetBool("moving", true);
+      }else{
+        animator.SetBool("moving", false);
       }
-
     }
 
     void MoveCharacter(){
